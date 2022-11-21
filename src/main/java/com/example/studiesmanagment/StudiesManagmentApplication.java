20480355b98323972ab74e3d427f87eb2b86e96b
hubert123 <hubert.antoniszyn98@gmail.com>
2022-11-21 16:12:45 +0100

    initial commit

---
+++ src/main/java/com/example/studiesmanagment/StudiesManagmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.studiesmanagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudiesManagmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudiesManagmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/studiesmanagment/StudiesManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studiesmanagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudiesManagmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
